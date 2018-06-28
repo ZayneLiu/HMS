9f2c9e405cf00e588352ccee29a495db12ff4b7d
1851839283 <1851839283@qq.com>
2018-06-28 18:46:37 +0800

    添加登陆检查

---
+++ BLL/Login.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    class Login
+    {
+       public static bool  is_login_valid()
+        {
+            return true;
+        }
+    }
+}
