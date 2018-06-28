6af3f5cfa01b0b169e271660bcd7b4a00c2ff30d
songjunzhe <40508750+songjunzhe@users.noreply.github.com>
2018-06-28 18:53:57 +0800

    修改

--- UI/patientLogin.cs
+++ UI/patientLogin.cs
@@ -6,9 +6,9 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using 
 using System.Windows.Forms;
 using System.Data.SqlClient;
+
 
 namespace UI
 {
@@ -21,7 +21,7 @@ namespace UI
         public Doctor myPartent;
         private void button1_Click(object sender, EventArgs e)
         {
-            if (DAL.is_login_valid(textBox1.Text, textBox2.Text))
+            if (BLL.Login.is_login_valid(textBox1.Text, textBox2.Text))
             {
                 patient frm = new patient();
                 frm.MdiParent = this.myPartent;
