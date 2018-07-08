2ee6b2ec96d89683449219b92e94e06d952c3324
songjunzhe <40508750+songjunzhe@users.noreply.github.com>
2018-07-09 06:01:52 +0800

    k

--- HMS_Patient/P_Login.cs
+++ HMS_Patient/P_Login.cs
@@ -19,12 +19,16 @@ namespace HMS_Patient
 
         private void Btn_Login_Click(object sender, EventArgs e)
         {
-            if (Server.Logics.Doctor_Logics.Is_Login_Info_Valid("用户名", "密码"))
+            if (Server.Logics.Patient_Logics.Is_Login_Info_Valid("用户名", "密码"))
             {
                 //P_Center frm = new P_Center();
                 //frm.Show();
-            };
-            MessageBox.Show("用户名或密码错误");
+            }
+            else
+            {
+                MessageBox.Show("用户名或密码错误");
+
+            }
         }
 
         private void Btn_Sign_Click(object sender, EventArgs e)
