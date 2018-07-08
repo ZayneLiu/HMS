afd0b8651413ecdd9ddb9368bfd4c854628fc158
songjunzhe <40508750+songjunzhe@users.noreply.github.com>
2018-07-09 05:45:53 +0800

    加入窗体

---
+++ HMS_Patient/P_Login.designer.cs
@@ -0,0 +1,174 @@
+﻿namespace HMS_Patient
+{
+    partial class P_Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(P_Login));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Btn_Sign = new System.Windows.Forms.Button();
+            this.Btn_Login = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Tbx_Username = new System.Windows.Forms.TextBox();
+            this.Tbx_Password = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.Btn_Sign);
+            this.panel1.Controls.Add(this.Btn_Login);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.Tbx_Username);
+            this.panel1.Controls.Add(this.Tbx_Password);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Location = new System.Drawing.Point(189, 114);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(295, 152);
+            this.panel1.TabIndex = 11;
+            // 
+            // Btn_Sign
+            // 
+            this.Btn_Sign.BackColor = System.Drawing.Color.Transparent;
+            this.Btn_Sign.FlatAppearance.BorderSize = 0;
+            this.Btn_Sign.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Btn_Sign.Font = new System.Drawing.Font("微软雅黑", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn_Sign.Location = new System.Drawing.Point(191, 99);
+            this.Btn_Sign.Name = "Btn_Sign";
+            this.Btn_Sign.Size = new System.Drawing.Size(86, 50);
+            this.Btn_Sign.TabIndex = 7;
+            this.Btn_Sign.Text = "注册";
+            this.Btn_Sign.UseVisualStyleBackColor = false;
+            this.Btn_Sign.Click += new System.EventHandler(this.Btn_Sign_Click);
+            // 
+            // Btn_Login
+            // 
+            this.Btn_Login.BackColor = System.Drawing.Color.Transparent;
+            this.Btn_Login.FlatAppearance.BorderSize = 0;
+            this.Btn_Login.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Btn_Login.Font = new System.Drawing.Font("微软雅黑", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn_Login.Location = new System.Drawing.Point(88, 99);
+            this.Btn_Login.Name = "Btn_Login";
+            this.Btn_Login.Size = new System.Drawing.Size(86, 50);
+            this.Btn_Login.TabIndex = 6;
+            this.Btn_Login.Text = "登陆";
+            this.Btn_Login.UseVisualStyleBackColor = false;
+            this.Btn_Login.Click += new System.EventHandler(this.Btn_Login_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(6, 14);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 35);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "账号:";
+            // 
+            // Tbx_Username
+            // 
+            this.Tbx_Username.Font = new System.Drawing.Font("Microsoft YaHei UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Tbx_Username.Location = new System.Drawing.Point(88, 14);
+            this.Tbx_Username.Name = "Tbx_Username";
+            this.Tbx_Username.Size = new System.Drawing.Size(199, 38);
+            this.Tbx_Username.TabIndex = 1;
+            // 
+            // Tbx_Password
+            // 
+            this.Tbx_Password.Font = new System.Drawing.Font("Microsoft YaHei UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Tbx_Password.Location = new System.Drawing.Point(88, 58);
+            this.Tbx_Password.Name = "Tbx_Password";
+            this.Tbx_Password.PasswordChar = '*';
+            this.Tbx_Password.Size = new System.Drawing.Size(199, 38);
+            this.Tbx_Password.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft YaHei UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(6, 58);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 35);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "密码:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(137, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(220, 64);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "患者登陆";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(17, 114);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(146, 152);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // P_Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(496, 285);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "P_Login";
+            this.Text = "登陆";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button Btn_Sign;
+        private System.Windows.Forms.Button Btn_Login;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Tbx_Username;
+        private System.Windows.Forms.TextBox Tbx_Password;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
---
+++ HMS_Patient/P_Register.designer.cs
@@ -0,0 +1,241 @@
+﻿namespace HMS_Patient
+{
+    partial class P_Register
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Tbx_Username = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Tbx_Sickness_History = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.Tbx_Tell = new System.Windows.Forms.TextBox();
+            this.Cbx_Gender = new System.Windows.Forms.ComboBox();
+            this.Tbx_Age = new System.Windows.Forms.TextBox();
+            this.Tbx_Turename = new System.Windows.Forms.TextBox();
+            this.Tbx_Password = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Tbx_Username
+            // 
+            this.Tbx_Username.Location = new System.Drawing.Point(250, 107);
+            this.Tbx_Username.Name = "Tbx_Username";
+            this.Tbx_Username.Size = new System.Drawing.Size(131, 21);
+            this.Tbx_Username.TabIndex = 62;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(148, 101);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 28);
+            this.label1.TabIndex = 61;
+            this.label1.Text = "用户名：";
+            // 
+            // Tbx_Sickness_History
+            // 
+            this.Tbx_Sickness_History.Location = new System.Drawing.Point(250, 314);
+            this.Tbx_Sickness_History.Name = "Tbx_Sickness_History";
+            this.Tbx_Sickness_History.Size = new System.Drawing.Size(131, 21);
+            this.Tbx_Sickness_History.TabIndex = 60;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("微软雅黑", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label10.Location = new System.Drawing.Point(126, 309);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(117, 28);
+            this.label10.TabIndex = 59;
+            this.label10.Text = "重大病史：";
+            // 
+            // Tbx_Tell
+            // 
+            this.Tbx_Tell.Location = new System.Drawing.Point(250, 278);
+            this.Tbx_Tell.Name = "Tbx_Tell";
+            this.Tbx_Tell.Size = new System.Drawing.Size(131, 21);
+            this.Tbx_Tell.TabIndex = 58;
+            // 
+            // Cbx_Gender
+            // 
+            this.Cbx_Gender.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.Cbx_Gender.FormattingEnabled = true;
+            this.Cbx_Gender.Items.AddRange(new object[] {
+            "男",
+            "女"});
+            this.Cbx_Gender.Location = new System.Drawing.Point(250, 249);
+            this.Cbx_Gender.Name = "Cbx_Gender";
+            this.Cbx_Gender.Size = new System.Drawing.Size(131, 20);
+            this.Cbx_Gender.TabIndex = 57;
+            // 
+            // Tbx_Age
+            // 
+            this.Tbx_Age.Location = new System.Drawing.Point(250, 218);
+            this.Tbx_Age.Name = "Tbx_Age";
+            this.Tbx_Age.Size = new System.Drawing.Size(131, 21);
+            this.Tbx_Age.TabIndex = 56;
+            // 
+            // Tbx_Turename
+            // 
+            this.Tbx_Turename.Location = new System.Drawing.Point(250, 183);
+            this.Tbx_Turename.Name = "Tbx_Turename";
+            this.Tbx_Turename.Size = new System.Drawing.Size(131, 21);
+            this.Tbx_Turename.TabIndex = 55;
+            // 
+            // Tbx_Password
+            // 
+            this.Tbx_Password.Location = new System.Drawing.Point(249, 149);
+            this.Tbx_Password.Name = "Tbx_Password";
+            this.Tbx_Password.PasswordChar = '*';
+            this.Tbx_Password.Size = new System.Drawing.Size(131, 21);
+            this.Tbx_Password.TabIndex = 54;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("微软雅黑", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label6.Location = new System.Drawing.Point(127, 281);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(117, 28);
+            this.label6.TabIndex = 53;
+            this.label6.Text = "联系方式：";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("微软雅黑", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label5.Location = new System.Drawing.Point(169, 245);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(75, 28);
+            this.label5.TabIndex = 52;
+            this.label5.Text = "性别：";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("微软雅黑", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label4.Location = new System.Drawing.Point(169, 211);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 28);
+            this.label4.TabIndex = 51;
+            this.label4.Text = "年龄：";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("微软雅黑", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(127, 183);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(117, 28);
+            this.label3.TabIndex = 50;
+            this.label3.Text = "真实姓名：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微软雅黑", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(168, 146);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 28);
+            this.label2.TabIndex = 49;
+            this.label2.Text = "密码：";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(52, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 63;
+            this.button1.Text = "返回";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(402, 27);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 64;
+            this.button2.Text = "确定";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // P_Register
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(500, 367);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Tbx_Username);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Tbx_Sickness_History);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.Tbx_Tell);
+            this.Controls.Add(this.Cbx_Gender);
+            this.Controls.Add(this.Tbx_Age);
+            this.Controls.Add(this.Tbx_Turename);
+            this.Controls.Add(this.Tbx_Password);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Name = "P_Register";
+            this.Text = "注册";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Tbx_Username;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Tbx_Sickness_History;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox Tbx_Tell;
+        private System.Windows.Forms.ComboBox Cbx_Gender;
+        private System.Windows.Forms.TextBox Tbx_Age;
+        private System.Windows.Forms.TextBox Tbx_Turename;
+        private System.Windows.Forms.TextBox Tbx_Password;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
