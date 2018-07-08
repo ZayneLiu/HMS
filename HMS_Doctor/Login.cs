e81245b5f11e869543dbf12d4680cbaa6dbae678
1851839283 <1851839283@qq.com>
2018-07-09 06:15:00 +0800

    医生登录

--- HMS_Doctor/Login.cs
+++ HMS_Doctor/Login.cs
@@ -16,5 +16,18 @@ namespace Doctor
         {
             InitializeComponent();
         }
+
+        private void Btn_Login_Click(object sender, EventArgs e)
+        {
+            if (Server.Logics.Doctor_Logics.Is_Login_Info_Valid(Tbx_Username.Text, Tbx_Password.Text))
+            {
+                Doc_Personal_Center frm = new Doc_Personal_Center();
+                frm.Show();
+            }
+            else 
+            {
+                MessageBox.Show("用户名或密码错误！");
+            }
+        }
     }
 }
