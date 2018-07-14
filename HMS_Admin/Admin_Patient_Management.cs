434c558920ae9e700c6878f2d27603abfb788a81
songjunzhe <40508750+songjunzhe@users.noreply.github.com>
2018-07-14 20:10:03 +0800

    1

---
+++ HMS_Admin/Admin_Patient_Management.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HMS_Partial
+{
+    public partial class Admin_Patient_Management : Form
+    {
+        public Form parent;
+        public Admin_Patient_Management(Form form)
+        {
+            InitializeComponent();
+            parent = form;
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            parent.Show();
+        }
+    }
+}
