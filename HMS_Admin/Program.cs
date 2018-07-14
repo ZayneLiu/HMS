513f1164c5665ad9f8bbd3d78738ef90f20a36e3
songjunzhe <40508750+songjunzhe@users.noreply.github.com>
2018-07-14 16:36:06 +0800

    admin 启动窗体

--- HMS_Admin/Program.cs
+++ HMS_Admin/Program.cs
@@ -16,8 +16,8 @@ namespace HMS_Partial
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            //Application.Run(new Admin_Login());
-            Application.Run(new Med_Management.Meds_Management_Landing_Page(new Admin_Landing_Page(new Form())));
+            Application.Run(new Admin_Login());
+            //Application.Run(new Med_Management.Meds_Management_Landing_Page(new Admin_Landing_Page(new Form())));
         }
     }
 }
